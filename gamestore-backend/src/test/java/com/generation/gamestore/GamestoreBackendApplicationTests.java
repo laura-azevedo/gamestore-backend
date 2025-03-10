f1f48401aadf1a1013510cb2d792a1a3b9db05bf
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-03-10 08:59:42 -0300

    chore: spring project config

---
+++ gamestore-backend/src/test/java/com/generation/gamestore/GamestoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.gamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamestoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

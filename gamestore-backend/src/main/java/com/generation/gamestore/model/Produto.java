263be03e5cac88eae8ab88ae849104db91178796
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-03-10 13:49:54 -0300

    feat: implement CategoriaRepository and ProdutoRepository

--- gamestore-backend/src/main/java/com/generation/gamestore/model/Produto.java
+++ gamestore-backend/src/main/java/com/generation/gamestore/model/Produto.java
@@ -6,7 +6,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Min;
@@ -14,7 +13,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
-@Table(name="tb_produto")
+@Table(name="tb_produto") 
 public class Produto {
 	
 	@Id

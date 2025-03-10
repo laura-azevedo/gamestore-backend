c82cc02502a7123714b89cd8dca70c69976a9601
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-03-10 15:15:30 -0300

    feat: implement delete method in CategoriaController

--- gamestore-backend/src/main/java/com/generation/gamestore/controller/CategoriaController.java
+++ gamestore-backend/src/main/java/com/generation/gamestore/controller/CategoriaController.java
@@ -1,18 +1,22 @@
 package com.generation.gamestore.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.gamestore.model.Categoria;
 import com.generation.gamestore.repository.CategoriaRepository;
@@ -60,5 +64,14 @@ public class CategoriaController {
 	                return ResponseEntity.ok(categoriaRepository.save(resposta));
 	            })
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+	}
+	
+	@ResponseStatus(HttpStatus.NO_CONTENT) 
+	@DeleteMapping("/{id}") 
+	public void delete(@PathVariable Long id) { 
+		Optional<Categoria> categoria = categoriaRepository.findById(id); 
+		if(categoria.isEmpty()) 
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND); 
+		categoriaRepository.deleteById(id); 
 	}
 }

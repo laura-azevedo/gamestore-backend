0f4807c044fbd47f60f9ec8feff58aca06e665c3
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-03-10 14:08:38 -0300

    feat: edit repository

--- gamestore-backend/src/main/java/com/generation/gamestore/controller/CategoriaController.java
+++ gamestore-backend/src/main/java/com/generation/gamestore/controller/CategoriaController.java
@@ -1,5 +1,12 @@
 package com.generation.gamestore.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/categorias")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class CategoriaController {
 
 }
